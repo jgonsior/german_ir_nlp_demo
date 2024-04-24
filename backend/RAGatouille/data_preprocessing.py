01aafd3c60b849bbaa45d04536e7bee2ec87e747
flml293c <flml293c@i8004.alpha.hpc.tu-dresden.de>
2024-04-24 13:07:03 +0200

    python scripts for preprocessing GermanDPR and training ColBERT on it

---
+++ backend/RAGatouille/data_preprocessing.py
@@ -0,0 +1,73 @@
+import json
+import pandas as pd
+import random
+
+TRAINING_DATA_PATH_GERMAN_DPR = "data/question_answer_pairs/GermanDPR/GermanDPR_train.json"
+TEST_DATA_PATH_GERMAN_DPR = "data/question_answer_pairs/GermanDPR/GermanDPR_test.json"
+
+def get_raw_data_GermanDPR(data_path):
+    '''
+    used format tiples: list of triplets of list of strings [([q_string], [p_string], [n1_string, n2_string, n3_string]), ... ]
+    used format corpus: list of strings
+    '''
+    with open(data_path, "r", encoding="utf-8") as train_file:
+        data = json.load(train_file)
+    
+    triples = []
+    corpus = []
+
+    for q in data:
+        query = q["question"]
+        positive_ctxs = [passage["text"].replace('\n', '\\n') for passage in q["positive_ctxs"]]
+
+        hard_negative_ctxs = [passage["text"].replace('\n', '\\n') for passage in q["hard_negative_ctxs"]]
+        triples.append([query, positive_ctxs,hard_negative_ctxs])
+        corpus += positive_ctxs
+        corpus += hard_negative_ctxs
+
+    return triples, corpus
+
+def create_GermanDPR_train_files(training_data_path, triple_path, passages_path):
+    triples, passages = get_raw_data_GermanDPR(training_data_path)
+    df_t = pd.DataFrame(triples, columns=["question", "positive_contexts", "negative_contexts"])
+    df_t.to_json(triple_path, orient="records", lines=True)
+    print(pd.read_json(triple_path, lines = True))
+    passages_df = pd.DataFrame(list(set(passages)))
+    passages_df.to_csv(passages_path, index=False, header=False)
+
+def create_GermanDPR_test_qa_file(test_data_path, qa_path):
+    qa_pairs = []
+    with open(test_data_path, "r", encoding="utf-8") as test_file:
+        data = json.load(test_file)
+
+    for q in data:
+        positive_ctxs = q["positive_ctxs"][0]["text"].replace('\n', '\\n')
+        qa_pairs.append([q["question"], positive_ctxs])
+    
+    df_qa_pairs = pd.DataFrame(qa_pairs, columns=["question", "answer"])
+    df_qa_pairs.to_json(qa_path, orient="records", lines=True)
+    print(pd.read_json(qa_path, lines = True))
+    
+def create_full_passage_corpus(passages_path, full_corpus_output_path):
+    _, train_corpus = get_raw_data_GermanDPR(TRAINING_DATA_PATH_GERMAN_DPR)
+    _, test_corpus = get_raw_data_GermanDPR(TEST_DATA_PATH_GERMAN_DPR)
+    passages_df = pd.read_csv(passages_path, header=None)
+    passages = list(set(passages_df[1].tolist() + train_corpus + test_corpus))
+
+    passages_df = pd.DataFrame(passages)
+    passages_df.to_csv(full_corpus_output_path, index=False, header=False)
+
+if __name__ == "__main__":
+    # creates triples for training (q, [p+], [p-,p-,p-]) in json format
+    create_GermanDPR_train_files(TRAINING_DATA_PATH_GERMAN_DPR, 
+            "data/question_answer_pairs/GermanDPR/train_triples.jsonl",
+            "data/question_answer_pairs/GermanDPR/train_passages.csv")
+
+    # creates the qa pairs for testing (evaluation)
+    create_GermanDPR_test_qa_file(TEST_DATA_PATH_GERMAN_DPR, 
+            "data/question_answer_pairs/GermanDPR/test_qa_pairs.jsonl") 
+
+    # creates the full passage corpus which is used for evaluation/retrieval (has to be indexed) 
+    #create_full_passage_corpus(OLD_PASSAGES_PATH, OLD_FULL_CORPUS_PATH)
+    #create_full_passage_corpus(NEW_PASSAGES_PATH, NEW_FULL_CORPUS_PATH)
+    
---
+++ backend/RAGatouille/training.py
@@ -0,0 +1,94 @@
+from ragatouille import RAGTrainer
+import pandas as pd
+import os
+import shutil
+
+BASE_MODEL_NAME = "bert-base-german-cased"
+TRAINING_DATA_NAME = "GermanDPR"
+TRIPLES_PATH = "data/question_answer_pairs/GermanDPR/train_triples.jsonl" 
+CORPUS_PATH = "data/question_answer_pairs/GermanDPR/train_passages.csv" 
+EPOCHS = [1,2] # training has to start with epoch 1. this variable currently only states how many epochs are trained
+
+def highest_alphabetical_directory(current_path):
+    directories = [d for d in os.listdir(current_path) if os.path.isdir(os.path.join(current_path, d))]
+    
+    if not directories:
+        return None
+
+    highest_directory = max(directories)
+    return highest_directory
+
+def most_recent_created_path(base_path):
+    current_path = base_path
+    for i in range(3):
+        current_path = os.path.join(current_path, highest_alphabetical_directory(current_path))
+    
+    return current_path
+
+def move_content(source_path, destination_path):
+    # Check if source path exists
+    if not os.path.exists(source_path):
+        print(f"Source path '{source_path}' does not exist.")
+        return
+    
+    # Check if destination path exists, create it if it doesn't
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+    
+    # Iterate through each item in the source path
+    for item in os.listdir(source_path):
+        # Create full paths for items in source and destination
+        source_item_path = os.path.join(source_path, item)
+        destination_item_path = os.path.join(destination_path, item)
+        
+        # If it's a file, move it to the destination
+        if os.path.isfile(source_item_path):
+            shutil.move(source_item_path, destination_item_path)
+            print(f"Moved file '{item}' to '{destination_path}'.")
+        # If it's a directory, recursively call move_content
+        elif os.path.isdir(source_item_path):
+            move_content(source_item_path, destination_item_path)
+    
+    # After moving all content, remove the source directory
+    os.rmdir(source_path)
+    print(f"Removed source directory '{source_path}'.")
+
+if __name__ == "__main__":
+    corpus = pd.read_csv(CORPUS_PATH).values.tolist()
+    trained_model_from_previous_epoch = None
+
+    triples = pd.read_json(TRIPLES_PATH, lines=True).values.tolist()
+    MODEL_NAME = f"{BASE_MODEL_NAME}-{TRAINING_DATA_NAME}"
+    for e in EPOCHS:
+        if e == EPOCHS[0]:
+            pretrained_model_name_or_path = BASE_MODEL_NAME
+        else:
+            pretrained_model_name_or_path = trained_model_from_previous_epoch
+
+        trainer = RAGTrainer(model_name = MODEL_NAME,
+                pretrained_model_name = pretrained_model_name_or_path,
+                language_code="de")
+
+        # This step handles all the data processing, check the examples for more details!
+        trainer.prepare_training_data(raw_data=triples,
+                                        all_documents = corpus,
+                                        data_out_path=f".ragatouille/training_data/{BASE_MODEL_NAME}/{TRAINING_DATA_NAME}/epoch{str(e)}", 
+                                        num_new_negatives = 0, 
+                                        mine_hard_negatives=False)
+
+        model_output_path = trainer.train(
+                batch_size=32,
+                nbits=4, # How many bits will the trained model use when compressing indexes
+                maxsteps=500000, # Maximum steps hard stop
+                use_ib_negatives=True, # Use in-batch negative to calculate loss
+                dim=128, # How many dimensions per embedding. 128 is the default and works well.
+                learning_rate=5e-6, # Learning rate, small values ([3e-6,3e-5] work best if the base model is BERT-like, 5e-6 is often the sweet spot)
+                doc_maxlen=512, # Maximum document length. Because of how ColBERT works, smaller chunks (128-256) work very well.
+                use_relu=False, # Disable ReLU -- doesn't improve performance
+                warmup_steps="auto", # Defaults to 10%
+                )
+        
+        # original colbert code forgot to propagate model_output_path -> hence we have to find it our selves
+        trained_model_from_previous_epoch = f".ragatouille/colbert/checkpoints/{BASE_MODEL_NAME}/{TRAINING_DATA_NAME}/epoch{str(e)}"
+        move_content(os.path.join(most_recent_created_path(".ragatouille/colbert/none"), "checkpoints/colbert"),
+                        trained_model_from_previous_epoch)
