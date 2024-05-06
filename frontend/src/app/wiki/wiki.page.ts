bcdb261a65d941e67e0fb449397085c1bc4cf6f0
Hannes <hannes.lienig@t-online.de>
2024-05-06 10:58:15 +0200

    Add initial WikiPage logic to wikipage

--- frontend/src/app/wiki/wiki.page.ts
+++ frontend/src/app/wiki/wiki.page.ts
@@ -1,7 +1,9 @@
 import {Component, inject, OnInit} from "@angular/core";
 
-import { DataService, Message} from "../services/data.service";
-import {RefresherCustomEvent} from "@ionic/angular";
+import { DataService } from "../services/data.service";
+import {Platform} from "@ionic/angular";
+import {QueryResponseResult} from "../types/query-response.type";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: "app-wiki",
@@ -9,19 +11,27 @@ import {RefresherCustomEvent} from "@ionic/angular";
   styleUrls: ["./wiki.page.scss"],
 })
 export class WikiPage {
-  private data: DataService = inject(DataService);
-  //protected queryResults: QueryResponseResult[] = [];
+  public docname: String;
+  public document!: String;
+  public queryResult!: QueryResponseResult;
+  private data = inject(DataService);
+  private activatedRoute = inject(ActivatedRoute);
+  private platform = inject(Platform);
 
-  constructor(private dataService: DataService) {}
+  constructor() {}
 
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000)
+  ngOnInit() {
+    this.docname = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    const idx = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.queryResult = this.activatedRoute.snapshot.paramMap.get('answer') as unknown as QueryResponseResult;
+    this.data.getDocomentById(parseInt(idx, 10)).then((res) => {
+      this.document = res;
+    });
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages()
+  getBackButtonText() {
+    const isIos = this.platform.is('ios')
+    return isIos ? 'Inbox' : '';
   }
 
 }
