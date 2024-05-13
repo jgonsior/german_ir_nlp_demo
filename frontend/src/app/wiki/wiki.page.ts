134e2ebb408bf02dcb09715d10171cae82faa6c7
Hannes <hannes.lienig@t-online.de>
2024-05-13 12:12:03 +0200

    Add view logic to WikiPage

--- frontend/src/app/wiki/wiki.page.ts
+++ frontend/src/app/wiki/wiki.page.ts
@@ -1,8 +1,11 @@
 import {Component, inject, OnInit} from "@angular/core";
 
-import { DataService } from "../services/data.service";
+import {DataService} from "../services/data.service";
 import {Platform} from "@ionic/angular";
-import {QueryResponseResult, QueryResponseDocument, QueryResponsePage} from "../types/query-response.type";
+import {
+  QueryResponseResult,
+  QueryResponsePage,
+} from "../types/query-response.type";
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
@@ -10,46 +13,38 @@ import {ActivatedRoute} from "@angular/router";
   templateUrl: "./wiki.page.html",
   styleUrls: ["./wiki.page.scss"],
 })
-export class WikiPage implements OnInit{
-  public doc_name: String;
-  public document: QueryResponsePage;
+export class WikiPage implements OnInit {
+  public docName: String;
+  public wikiPage!: QueryResponsePage;
   public queryResult!: QueryResponseResult;
-  public sorted_doc: Map<string[], string[]> = new Map();
+  public createdHeaders: string[] = [];
   private data = inject(DataService);
   private activatedRoute = inject(ActivatedRoute);
   private platform = inject(Platform);
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {
-    this.doc_name = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.docName = this.activatedRoute.snapshot.paramMap.get('id') as string;
     const idx = this.activatedRoute.snapshot.paramMap.get('id') as string;
     this.queryResult = this.activatedRoute.snapshot.paramMap.get('answer') as unknown as QueryResponseResult;
     this.data.getDocomentById(parseInt(idx, 10)).then((res) => {
-      this.document = res;
-      console.log(this.document)
+      this.wikiPage = res;
     });
-
-    let current_head: string[] = [];
-    let passages: string[] = [];
-
-    for (let page of this.document.page) {
-      if (current_head == page.headers) {
-        passages.push(page.passage);
-      }
-      else {
-        this.sorted_doc.set(current_head,passages);
-        current_head = page.headers;
-        passages = [];
-        passages.push(page.passage);
-      }
-    }
-
   }
 
   getBackButtonText() {
     const isIos = this.platform.is('ios')
     return isIos ? 'Inbox' : '';
+  }
+
+  addCreatedHeader(header: string) {
+    this.createdHeaders.push(header);
+  }
+
+  isHeaderAlreadyCreated(header: string): boolean {
+    return this.createdHeaders.includes(header);
   }
 
 }
