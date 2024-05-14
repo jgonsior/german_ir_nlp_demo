f7710bf6f47f9f18824479caed433d9cb61e669e
Tommy Nguyen <thinktommy@gmx.de>
2024-05-15 00:34:22 +0200

    corpus.py creates corpus suited for gpl training

--- backend/query_generation/corpus.py
+++ backend/query_generation/corpus.py
@@ -3,21 +3,24 @@ import json
 
 def create_corpus():
 
-    id = 1
     corpus = []
     with open('backend/query_generation/hp-gpl/harry_potter_unicode_processed.json', 'r') as file:
         data = json.load(file)
         for wikipage in data:
+            page_id = wikipage['id']
+            paragraph_id = 0
             for paragraph in wikipage['text']:
                 doc = {}
-                doc['id'] = id
+                doc['_id'] = f'{page_id}-{paragraph_id}'
                 doc['title'] = wikipage['title']
                 doc['text'] = paragraph
-                id += 1
+                paragraph_id += 1
                 corpus.append(doc)
 
-    with open('backend/query_generation/hp-gpl/corpus.json', 'w', encoding='utf-8') as file:
-        json.dump(corpus, file, indent=1, ensure_ascii=False)
+    with open('backend/query_generation/hp-gpl/corpus.jsonl', 'w', encoding='utf-8') as file:
+        for obj in corpus:
+            json.dump(obj, file, ensure_ascii=False)
+            file.write('\n')
 
 if __name__ == "__main__":
     create_corpus()
