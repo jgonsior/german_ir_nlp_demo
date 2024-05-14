c0818097a6cb71f3702f580dcd7468d2e47a1844
Tommy Nguyen <thinktommy@gmx.de>
2024-05-14 14:07:14 +0200

    script for inverted index

---
+++ backend/inv_index/inverted_index.py
@@ -0,0 +1,36 @@
+import nltk
+import json
+
+def clean_tokens(tokenlist):
+    remove_chars = ['"', ',', '.', '[', ']', '{', '}', '(', ')', ':']
+    tokens = [token.lower() for token in tokenlist if token not in remove_chars]        
+
+    return tokens
+
+def calculate_inverted_index():
+        inverted_index = {}
+
+        with open('backend/inv_index/harry_potter.json', 'r') as file:
+            data = json.load(file)
+
+        for document in data:
+            raw_tokens = set(nltk.word_tokenize(document["text"]))
+            tokens = clean_tokens(raw_tokens)
+            for token in tokens:
+                if token not in inverted_index:
+                    inverted_index[token] = []
+                inverted_index[token].append(document['id'])
+
+        with open('backend/inv_index/inverted_index.json', 'w', encoding="utf-8") as output:
+                json.dump(inverted_index, output, indent=1, ensure_ascii=False)
+
+def get_all_tokens():
+    with open('backend/inv_index/inverted_index.json', 'r', encoding="utf-8") as file:
+        data = json.load(file)
+    
+    return data.keys()
+
+
+if __name__ == '__main__':
+    #calculate_inverted_index()
+    print(get_all_tokens())
