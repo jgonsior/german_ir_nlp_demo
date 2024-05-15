b08d489266b6e6b8cc729e21d7d4168cb6bd1aa7
Tommy Nguyen <thinktommy@gmx.de>
2024-05-15 19:11:48 +0200

    lemmatize and stemm the inverted index

--- backend/inv_index/inverted_index.py
+++ backend/inv_index/inverted_index.py
@@ -1,11 +1,12 @@
 import nltk
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
+import spacy
 import json
 
 def clean_tokens(tokenlist):
 
-    remove_chars = ['"', ',', '.', '[', ']', '{', '}', '(', ')', ':', '-', ';', "'"]
+    remove_chars = ['"', ',', '.', '[', ']', '{', '}', '(', ')', ':', '-', ';', "'", "!", '“', '„', '&', "''", "'s", '*', '...', '):', '/', '--']
     tokens_filtered = [token.lower() for token in tokenlist if token not in remove_chars]        
 
     stemmer = SnowballStemmer("german")
@@ -16,6 +17,8 @@ def clean_tokens(tokenlist):
 
 def calculate_inverted_index(wiki_pages, output: str):
         inverted_index = {}
+        #lemmas = set()
+        nlp = spacy.load("de_core_news_sm")
 
         with open('backend/inv_index/harry_potter.json', 'r') as file:
             data = json.load(file)
@@ -24,7 +27,12 @@ def calculate_inverted_index(wiki_pages, output: str):
             title = document['title']
 
             if title in wiki_pages:
-                raw_tokens = set(nltk.word_tokenize(document["text"]))
+                #raw_tokens = set(nltk.word_tokenize(document["text"]))
+
+                raw_tokens = [str(token.lemma_) for token in nlp(document["text"])]
+
+                #lemmas.update([(token, token.lemma_) for token in nlp(document["text"])])
+
                 tokens = clean_tokens(raw_tokens)
 
                 if document['title'] not in inverted_index.keys():
@@ -33,7 +41,10 @@ def calculate_inverted_index(wiki_pages, output: str):
                 for token in tokens:
                     if token not in inverted_index[title]:
                         inverted_index[title][token] = []
-                    inverted_index[title][token].append(document['id'])
+                    inverted_index[title][token].append(document['_id'])
+
+        # sorted(lemmas)
+        # print(lemmas)
 
         with open(f'backend/inv_index/{output}', 'w', encoding="utf-8") as output:
                 json.dump(inverted_index, output, indent=1, ensure_ascii=False)
@@ -54,5 +65,7 @@ if __name__ == '__main__':
     output = "inv_index_Harry.json"
     calculate_inverted_index(wiki_pages, output)
     tokens = get_all_tokens("inv_index_Harry.json")
+
+    tokens.sort()
     print(tokens)
     print(len(tokens))
