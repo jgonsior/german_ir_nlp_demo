00bc8488d618daacb907a7ece10dbd2a118744bc
Tommy Nguyen <thinktommy@gmx.de>
2024-05-15 16:17:15 +0200

    just Harry Potter page and stemming

--- backend/inv_index/inverted_index.py
+++ backend/inv_index/inverted_index.py
@@ -1,13 +1,20 @@
 import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
 import json
 
 def clean_tokens(tokenlist):
-    remove_chars = ['"', ',', '.', '[', ']', '{', '}', '(', ')', ':']
-    tokens = [token.lower() for token in tokenlist if token not in remove_chars]        
 
-    return tokens
+    remove_chars = ['"', ',', '.', '[', ']', '{', '}', '(', ')', ':', '-', ';', "'"]
+    tokens_filtered = [token.lower() for token in tokenlist if token not in remove_chars]        
 
-def calculate_inverted_index():
+    stemmer = SnowballStemmer("german")
+    stop_words = set(stopwords.words("german"))
+    tokens_stemmed = [stemmer.stem(token) for token in tokens_filtered if token not in stop_words]
+
+    return tokens_stemmed
+
+def calculate_inverted_index(wiki_pages, output: str):
         inverted_index = {}
 
         with open('backend/inv_index/harry_potter.json', 'r') as file:
@@ -15,27 +22,37 @@ def calculate_inverted_index():
 
         for document in data:
             title = document['title']
-            raw_tokens = set(nltk.word_tokenize(document["text"]))
-            tokens = clean_tokens(raw_tokens)
 
-            if document['title'] not in inverted_index.keys():
-                 inverted_index[title] = {}
+            if title in wiki_pages:
+                raw_tokens = set(nltk.word_tokenize(document["text"]))
+                tokens = clean_tokens(raw_tokens)
 
-            for token in tokens:
-                if token not in inverted_index[title]:
-                    inverted_index[title][token] = []
-                inverted_index[title][token].append(document['id'])
+                if document['title'] not in inverted_index.keys():
+                    inverted_index[title] = {}
 
-        with open('backend/inv_index/inverted_index.json', 'w', encoding="utf-8") as output:
+                for token in tokens:
+                    if token not in inverted_index[title]:
+                        inverted_index[title][token] = []
+                    inverted_index[title][token].append(document['id'])
+
+        with open(f'backend/inv_index/{output}', 'w', encoding="utf-8") as output:
                 json.dump(inverted_index, output, indent=1, ensure_ascii=False)
 
-def get_all_tokens():
-    with open('backend/inv_index/inverted_index.json', 'r', encoding="utf-8") as file:
+def get_all_tokens(file):
+    tokens = []
+    with open(f'backend/inv_index/{file}', 'r', encoding="utf-8") as file:
         data = json.load(file)
+ 
+    for obj in data.values():
+        tokens.extend(obj.keys())
     
-    return data.keys()
+    return tokens
 
 
 if __name__ == '__main__':
-    #calculate_inverted_index()
-    print(get_all_tokens())
+    wiki_pages=["Harry Potter"]
+    output = "inv_index_Harry.json"
+    calculate_inverted_index(wiki_pages, output)
+    tokens = get_all_tokens("inv_index_Harry.json")
+    print(tokens)
+    print(len(tokens))
