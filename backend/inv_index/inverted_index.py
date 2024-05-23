7cdcbfcd1888373c92e298db174cc40ef091fba1
jembie <75253705+jembie@users.noreply.github.com>
2024-05-23 14:57:21 +0200

    Add new corpus, inverted index for normalised and unnormalised

--- backend/inv_index/inverted_index.py
+++ backend/inv_index/inverted_index.py
@@ -1,16 +1,44 @@
-import nltk
-from nltk.stem.snowball import SnowballStemmer
-from nltk.corpus import stopwords
-import spacy
 import json
 
+import nltk
+import spacy
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+
 def clean_tokens(tokenlist, normalize: bool = True):
-    remove_chars = ['"', ',', '.', '[', ']', '{', '}', '(', ')', ':', '-', ';', "'", "!", '“', '„', '&', "''", "'s", '*', '...', '):', '/', '--', '---']
-    tokens_filtered = [token.lower() for token in tokenlist if token not in remove_chars]        
+    remove_chars = [
+        '"',
+        ",",
+        ".",
+        "[",
+        "]",
+        "{",
+        "}",
+        "(",
+        ")",
+        ":",
+        "-",
+        ";",
+        "'",
+        "!",
+        "“",
+        "„",
+        "&",
+        "''",
+        "'s",
+        "*",
+        "...",
+        "):",
+        "/",
+        "--",
+        "---",
+    ]
+    tokens_filtered = [token.lower() for token in tokenlist if token not in remove_chars]
 
     stemmer = SnowballStemmer("german")
     stop_words = set(stopwords.words("german"))
-    if normalize: 
+    if normalize:
         tokens = [stemmer.stem(token) for token in tokens_filtered if token not in stop_words]
         tokens = [stemmer.stem(token) for token in tokens if token not in stop_words]
     else:
@@ -20,69 +48,70 @@ def clean_tokens(tokenlist, normalize: bool = True):
 
 
 def calculate_inverted_index(wiki_pages, output: str, normalize: bool = True):
-        inverted_index = {}
-        #lemmas = set()
-        nlp = spacy.load("de_core_news_sm")
+    inverted_index = {}
+    # lemmas = set()
+    nlp = spacy.load("de_core_news_sm")
 
-        with open('backend/inv_index/harry_potter.json', 'r') as file:
-            data = json.load(file)
+    with open("backend/inv_index/corpus_wiki.json", "r") as file:
+        data = json.load(file)
 
-        for document in data:
-            title = document['title']
+    for document in data:
+        title = document["title"]
 
-            if title in wiki_pages:
-                
-                if normalize:
-                    raw_tokens = set(str(token.lemma_.lower()) for token in nlp(document["text"]))
-                    raw_tokens = set(str(token.lemma_.lower()) for token in nlp(" ".join(raw_tokens)))
-                else:
-                    raw_tokens = set(nltk.word_tokenize(document["text"]))
+        if title in wiki_pages:
 
-                tokens = clean_tokens(raw_tokens, normalize)
+            if normalize:
+                raw_tokens = set(str(token.lemma_.lower()) for token in nlp(document["text"]))
+                raw_tokens = set(str(token.lemma_.lower()) for token in nlp(" ".join(raw_tokens)))
+            else:
+                raw_tokens = set(nltk.word_tokenize(document["text"]))
 
-                #lemmas.update(set((token, token.lemma_) for token in nlp(" ".join(tokens))))
+            tokens = clean_tokens(raw_tokens, normalize)
 
-                if document['title'] not in inverted_index.keys():
-                    inverted_index[title] = {}
+            # lemmas.update(set((token, token.lemma_) for token in nlp(" ".join(tokens))))
 
-                for token in tokens:
-                    if token not in inverted_index[title]:
-                        inverted_index[title][token] = []
-                    if document['_id'] not in inverted_index[title][token]:
-                        inverted_index[title][token].append(document['_id'])
+            if document["title"] not in inverted_index.keys():
+                inverted_index[title] = {}
 
-        #sorted(lemmas)
-        #lemmas = '{' + ', '.join(map(str, lemmas)) + '}'
+            for token in tokens:
+                if token not in inverted_index[title]:
+                    inverted_index[title][token] = []
+                if document["_id"] not in inverted_index[title][token]:
+                    inverted_index[title][token].append(document["_id"])
 
-        # with open('backend/inv_index/lemma.txt', 'w') as f:
-        #     f.write(lemmas)
+    # sorted(lemmas)
+    # lemmas = '{' + ', '.join(map(str, lemmas)) + '}'
 
-        with open(f'backend/inv_index/{output}', 'w', encoding="utf-8") as output:
-                json.dump(inverted_index, output, indent=1, ensure_ascii=False)
+    # with open('backend/inv_index/lemma.txt', 'w') as f:
+    #     f.write(lemmas)
+
+    with open(f"backend/inv_index/{output}", "w", encoding="utf-8") as output:
+        json.dump(inverted_index, output, indent=1, sort_keys=True, ensure_ascii=False)
+
 
 def get_all_tokens(file):
     tokens = []
-    with open(f'backend/inv_index/{file}', 'r', encoding="utf-8") as file:
+    with open(f"backend/inv_index/{file}", "r", encoding="utf-8") as file:
         data = json.load(file)
- 
+
     for obj in data.values():
         tokens.extend(obj.keys())
-    
+
     return tokens
 
 
-if __name__ == '__main__':
-    wiki_pages=["Harry Potter"]
-    output = "inv_index_Harry.json"
-    calculate_inverted_index(wiki_pages, output, True)
+if __name__ == "__main__":
+    wiki_pages = ["Harry Potter", "Albus Dumbledore", "Tom Riddle", "Minerva McGonagall", "Severus Snape", "Schloss Hogwarts"]
+    output = "inv_index_wissenschaftskommunikation_normalized.json"
+    calculate_inverted_index(wiki_pages, output, normalize=True)
 
-    #tokens = get_all_tokens(output)
+    # tokens = get_all_tokens(output)
     # tokens_double = get_all_tokens("inv_index_double_Harry.json")
-    #tokens.sort()
+    # tokens.sort()
     # tokens_double.sort()
-    #print(tokens)
+    # print(tokens)
     # print(tokens_double)
-    #print(len(tokens))
+    # print(len(tokens))
     # print(len(tokens_double))
     # rem_token1 = [token for token in tokens if token not in tokens_double]
     # rem_token2 = [token for token in tokens_double if token not in tokens]
