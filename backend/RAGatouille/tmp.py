9167856f9a42067de690a061c5f5b6a59a405975
Tommy Nguyen <thinktommy@gmx.de>
2024-06-05 19:06:53 +0200

    embedding word function + embeddings tokens

---
+++ backend/RAGatouille/tmp.py
@@ -0,0 +1,34 @@
+import transformers as tf
+import numpy as np
+import pandas as pd
+
+tokenizer = tf.BertTokenizer.from_pretrained("/home/tommynguyen/VSCode/german_ir_nlp_demo/backend/data/colbert/checkpoints/bert-base-german-cased/GermanDPR-XQA-HP/epoch1")
+
+df = pd.read_csv("/home/tommynguyen/VSCode/german_ir_nlp_demo/embeddings.csv")
+
+def embed_word(tokenizer: tf.BertTokenizer, df:pd.DataFrame, word: str) -> np.ndarray:
+
+    tokens = tokenizer(word)["input_ids"][1:-1]
+
+    df['embedding'] = df['embedding'].apply(lambda x: np.fromstring(x.strip('[]'), sep=' '))
+    #df['embedding_mean'] = df['embedding'].apply(lambda x: sum(x)/len(x))
+
+    embedding = df.iloc[tokens]["embedding"].mean()
+    return embedding
+
+print(embed_word(tokenizer=tokenizer, df=df, word="Hufflepuff"))
+#print(embeddings.shape)
+#print(embeddings.mean().shape)#.mean(axis=0))
+
+
+# with open("backend/data/colbert/checkpoints/bert-base-german-cased/GermanDPR-XQA-HP/epoch1/vocab.txt", "r", encoding="utf-8") as tokens:
+#     lines = tokens.readlines()
+
+# lines = [line[:-1] for line in lines]
+
+
+# embeddings = [(token, list(RAG.model.inference_ckpt.bert.parameters())[0][i].detach().numpy()) for i, token in enumerate(lines)]
+# df = pd.DataFrame(embeddings, columns=["token", "embedding"])
+# print(df.head(10))
+
+# df.to_csv("embeddings.csv", index=False)
