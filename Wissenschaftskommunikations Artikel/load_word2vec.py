62fe6fc95876b5224384e8986728e5024949e519
jembie <75253705+jembie@users.noreply.github.com>
2024-06-05 21:41:44 +0200

    Update `word2vec.py` with more input samples

--- Wissenschaftskommunikations Artikel/load_word2vec.py
+++ Wissenschaftskommunikations Artikel/load_word2vec.py
@@ -6,8 +6,6 @@ from gensim.models import Word2Vec
 # Load the model
 model = Word2Vec.load("Wissenschaftskommunikations Artikel/harry_potter_german_word2vec.model")
 
-print(model.wv.most_similar("dumbledore"))
-
 plt.figure(figsize=(15, 4))
 sns.heatmap(
     [
@@ -16,21 +14,18 @@ sns.heatmap(
         model.wv["zauberer"],
         model.wv["hexen"],
         model.wv["hexe"],
+        model.wv["dumbledore"],
+        model.wv["schulleiter"],
     ],
     cmap=sns.color_palette("coolwarm", as_cmap=True),
     cbar=True,
     xticklabels=False,
-    yticklabels=[
-        "harry",
-        "potter",
-        "zauberer",
-        "hexen",
-        "hexe",
-    ],
+    yticklabels=["harry", "potter", "zauberer", "hexen", "hexe", "dumbledore", "schulleiter"],
     linewidths=1,
 )
 
 # Rotate y-tick labels to 90 degrees
 plt.yticks(rotation=0)
+plt.savefig("Wissenschaftskommunikations Artikel/word2vec.pdf", dpi="figure", format="pdf")
 
 plt.show()
