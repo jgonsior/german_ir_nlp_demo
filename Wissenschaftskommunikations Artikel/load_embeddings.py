9458c45b8d244eb006bc9a346ec8698b7ccebf8c
jembie <75253705+jembie@users.noreply.github.com>
2024-06-06 13:35:23 +0200

    Update `load_embeddings.py`

--- Wissenschaftskommunikations Artikel/load_embeddings.py
+++ Wissenschaftskommunikations Artikel/load_embeddings.py
@@ -4,22 +4,37 @@ import pandas as pd
 import seaborn as sns
 from sklearn.decomposition import PCA
 
-df = pd.read_csv("embeddings.csv")
+df = pd.read_csv("unity.csv")
 
 df["embedding"] = df["embedding"].apply(lambda x: np.fromstring(x.strip("[]"), sep=" "))
 
 arr = np.vstack(df["embedding"])
 
-decomp = PCA(n_components=100).fit_transform(arr)
+decomp = PCA(
+    n_components=100,
+).fit_transform(arr)
 
 
 plt.figure(figsize=(15, 4))
 sns.heatmap(
-    [decomp[0], decomp[1]],
+    [decomp[0], decomp[1], decomp[2], decomp[3], decomp[4], decomp[5], decomp[6], decomp[7], decomp[8], decomp[9], decomp[10], decomp[11]],
     cmap=sns.color_palette("coolwarm", as_cmap=True),
     cbar=True,
     xticklabels=False,
-    yticklabels=["harry", "potter", ""],
+    yticklabels=[
+        "Harry",
+        "Potter",
+        "Hermine",
+        "Hexe",
+        "Hexen",
+        "Zauberer",
+        "Dumbledore",
+        "Schulleiter",
+        "Waschmaschine",
+        "Fernseher",
+        "Mikrowelle",
+        "Wolfgang",
+    ],
     linewidths=1,
 )
 
