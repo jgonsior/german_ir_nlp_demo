c1e10dd4d8463c322e6100eea8e936dbbcf67e54
HannesLienig <hannes.lienig@t-online.de>
2024-06-12 19:13:26 +0200

    add dimension and answers to be more flexible in code and accommodate to recent changes

--- Wissenschaftskommunikations Artikel/barcodes/src/barcode_generation.py
+++ Wissenschaftskommunikations Artikel/barcodes/src/barcode_generation.py
@@ -14,12 +14,10 @@ import jinja2
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
-INVERTED_INDEX_PATH = "../invIndex/"
-
-nltk.download('stopwords')
+nltk.download("stopwords")
 stemmer = SnowballStemmer("german")
 stop_words = set(stopwords.words("german"))
-nlp = spacy.load('de_core_news_sm')
+nlp = spacy.load("de_core_news_sm")
 
 latex_jinja_env = jinja2.Environment(
     block_start_string=r"\BLOCK{",
@@ -42,122 +40,12 @@ def detect_os():
     return os_name
 
 
-def and_lists(list1, list2):
-    if len(list1) != len(list2):
-        raise ValueError("Both lists must have the same length")
-    return [a & b for a, b in zip(list1, list2)]
-
-
-def get_xor_and(dict_codes: dict) -> list:
-    lst_keys = list(dict_codes.keys())
-    if len(dict_codes) == 1:
-        print(
-            "You only provided one or zero question barcodes or document barcodes, the logic for "
-            "shortening them will be skipped!"
-        )
-        return dict_codes[lst_keys[0]]
-    elif len(dict_codes) == 0:
-        raise ValueError(
-            "You must provide at least one question barcode and document barcode"
-        )
-
-    for idx, barcode in enumerate(dict_codes):
-        if idx == 0:
-            and_barcode = and_lists(dict_codes[barcode], dict_codes[lst_keys[idx + 1]])
-        elif idx == len(dict_codes) - 2:
-            and_barcode = and_lists(and_barcode, dict_codes[lst_keys[idx + 1]])
-        elif idx < len(dict_codes) - 3:
-            and_barcode = and_lists(and_barcode, dict_codes[lst_keys[idx + 1]])
-
-    return and_barcode
-
-
-def remove_redundant(dict_codes: dict, lst_xor: list) -> dict:
-    for idx, num_xor in enumerate(lst_xor):
-        for barcode in dict_codes:
-            if num_xor == 1:
-                code = dict_codes[barcode]
-                code[idx] = ""
-                dict_codes[barcode] = code
-
-    for barcode in dict_codes:
-        dict_codes[barcode] = [i for i in dict_codes[barcode] if i != ""]
-
-    return dict_codes
-
-
-def fill_remaining_zeros(lst, result, max_length, ones_positions):
-    if len(result) < max_length:
-        if not ones_positions:
-            result.extend([0] * (max_length - len(result)))
-        else:
-            last_one_index = ones_positions[-1]
-            remaining_slots = max_length - len(result)
-            for i in range(last_one_index + 1, len(lst)):
-                if lst[i] == 0 and remaining_slots > 0:
-                    result.append(0)
-                    remaining_slots -= 1
-                if len(result) >= max_length:
-                    break
-
-    return result
-
-
-def barcode_minification(dict_codes: dict, max_length: int) -> dict:
-    for code in dict_codes:
-        lst = dict_codes[code]
-        result = []
-        zero_count = 0
-        ones_positions = [i for i, x in enumerate(lst) if x == 1]
-
-        ones_count = len(ones_positions)
-        max_zeros = (
-            (max_length - ones_count) // (ones_count - 1)
-            if ones_count > 1
-            else max_length - 1
-        )
-
-        for num in lst:
-            if num == 1:
-                result.append(num)
-                zero_count = 0
-            else:
-                zero_count += 1
-                if zero_count <= max_zeros:
-                    result.append(num)
-
-            if len(result) >= max_length:
-                break
-
-        if result.count(1) < ones_count:
-            result = []
-            zero_count = 0
-            for i, num in enumerate(lst):
-                if num == 1:
-                    result.append(num)
-                    zero_count = 0
-                else:
-                    zero_count += 1
-                    if zero_count <= max_zeros:
-                        result.append(num)
-                if len(result) >= max_length:
-                    break
-
-        if len(result) < max_length:
-            if ones_positions and ones_positions[-1] >= len(result):
-                result.extend([0] * (max_length - len(result)))
-            else:
-                result = fill_remaining_zeros(lst, result, max_length, ones_positions)
-
-        dict_codes[code] = result
-
-    return dict_codes
-
-
 def draw_svg(name: str, barcode: list, identity: str, height: int, width: int):
     if not os.path.exists(f"../barcodes_out/{identity}/svg/"):
         os.makedirs(f"../barcodes_out/{identity}/svg/")
-        print(f"created directory: ../barcodes_out/{identity}/svg/ to save the latex file to")
+        print(
+            f"created directory: ../barcodes_out/{identity}/svg/ to save the latex file to"
+        )
 
     if len(identity) == 9:
         identity_short = identity[:8]
@@ -165,9 +53,9 @@ def draw_svg(name: str, barcode: list, identity: str, height: int, width: int):
         identity_short = identity
 
     with cairo.SVGSurface(
-            f"../barcodes_out/{identity}/svg/{identity_short}_{name}.svg",
-            (len(barcode) * width),
-            height,
+        f"../barcodes_out/{identity}/svg/{identity_short}_{name}.svg",
+        (len(barcode) * width),
+        height,
     ) as surface:
         context = cairo.Context(surface)
         for idx, numeral in enumerate(barcode):
@@ -200,11 +88,13 @@ def create_latex(switch: str):
 
     if not os.path.exists(f"../barcodes_out/{switch}/tex"):
         os.makedirs(f"../barcodes_out/{switch}/tex")
-        print(f"created directory: ../barcodes_out/{switch}/tex to save the latex file to")
+        print(
+            f"created directory: ../barcodes_out/{switch}/tex to save the latex file to"
+        )
 
     with open(
-            f"../barcodes_out/{switch}/tex/{switch}.tex",
-            "w",
+        f"../barcodes_out/{switch}/tex/{switch}.tex",
+        "w",
     ) as document_file:
         document_file.write(result_document)
 
@@ -215,7 +105,9 @@ def create_pdf(switch: str):
 
     if not os.path.exists(f"../barcodes_out/{switch}/pdf/"):
         os.makedirs(f"../barcodes_out/{switch}/pdf/")
-        print(f"created directory: ../barcodes_out/{switch}/pdf/ to save the pdf files to")
+        print(
+            f"created directory: ../barcodes_out/{switch}/pdf/ to save the pdf files to"
+        )
 
     os_name = detect_os()
 
@@ -250,11 +142,12 @@ def generate_dummy():
 
 
 def generate_barcodes(
-        index_file: str,
-        questions_file: str,
-        amount: int,
-        mode: str,
-        max_length: int,
+    index_file: str,
+    questions_file: str,
+    dimension_file: str,
+    answer_file: str,
+    amount: int,
+    mode: str,
 ):
     """
 
@@ -276,6 +169,12 @@ def generate_barcodes(
     with open(questions_file, "r", encoding="utf8") as infile:
         questions = json.load(infile)
 
+    if dimension_file:
+        with open(dimension_file, "r", encoding="utf8") as infile:
+            dimensions = json.load(infile)
+    if answer_file:
+        with open(answer_file, "r", encoding="utf8") as infile:
+            answers = json.load(infile)
     print("Done loading files...")
     print("Generating barcodes...")
 
@@ -298,10 +197,18 @@ def generate_barcodes(
     for question in questions:
         questions[question] = questions[question].replace("?", "")
 
-        question_raw = set(str(token.lemma_.lower()) for token in nlp(questions[question]))
-        question_raw = set(str(token.lemma_.lower()) for token in nlp(" ".join(question_raw)))
-        question_token = [stemmer.stem(token) for token in question_raw if token not in stop_words]
-        question_token = [stemmer.stem(token) for token in question_token if token not in stop_words]
+        question_raw = set(
+            str(token.lemma_.lower()) for token in nlp(questions[question])
+        )
+        question_raw = set(
+            str(token.lemma_.lower()) for token in nlp(" ".join(question_raw))
+        )
+        question_token = [
+            stemmer.stem(token) for token in question_raw if token not in stop_words
+        ]
+        question_token = [
+            stemmer.stem(token) for token in question_token if token not in stop_words
+        ]
 
         for word in inv_index:
             if word in question_token:
@@ -310,17 +217,10 @@ def generate_barcodes(
                 dict_questions[question].append(0)
 
     dict_codes = {}
-    lst_para = ['1-1', '1-46', '2-68', '2-59', '2-73', '3-2', '3-12', '3-24', '3-117', '3-200',
-                '4-2', '4-8', '4-13', '4-60', '4-68', '4-78', '6-1', '6-2', '6-77', '6-78']
+    lst_para = []
     for collection in dict_barcodes:
         if collection in lst_para:
             dict_codes[collection] = dict_barcodes[collection]
-
-    and_barcode_doc = get_xor_and(dict_codes)
-    dict_barcodes = remove_redundant(dict_codes, and_barcode_doc)
-
-    dict_barcodes = barcode_minification(dict_barcodes, max_length)
-    dict_questions = barcode_minification(dict_questions, max_length)
 
     if not os.path.isfile(f"../barcodes_out/dummy/svg/dummy.svg"):
         print("Generating dummy barcode...")
@@ -362,39 +262,61 @@ def generate_barcodes(
     print("DONE")
 
 
-def handler(amount: int, mode: str, max_length: int = 100):
-    if len(os.listdir(INVERTED_INDEX_PATH)) < 2:
+def handler(amount: int, mode: str):
+    if len(os.listdir("../invIndex/")) < 2:
         raise Exception(
-            "No inverted index or question files found. Please provide both in JSON format"
+            "No inverted index, question or dimension files found."
+            "Please provide all three in JSON format as specified in the README.md!"
         )
 
-    for file in os.listdir(INVERTED_INDEX_PATH):
+    for file in os.listdir("../invIndex/"):
         if file.endswith(".json"):
-            if "questions" not in file:
+            file_name = os.path.splitext(file)[0]
+            if file_name.lower() in ["invindex", "inverted_index", "inv_index"]:
+                print("Found index file.")
                 index_path = f"../invIndex/{file}"
-            else:
+            elif file_name.lower() in [
+                "questions",
+                "questions_index",
+                "questions_index",
+            ]:
+                print("Found questions file.")
                 questions_path = f"../invIndex/{file}"
+            elif file_name.lower() in [
+                "dimension",
+                "dimensions",
+                "dimensions_index",
+            ]:
+                print("Found dimension file.")
+                dimensions_path = f"../dimensions/{file}"
+            elif file_name.lower() in ["paragraphs", "answers", "answer_paragraphs"]:
+                print("Found paragraphs file.")
+                answers_path = f"../answers/{file}"
+            else:
+                print(
+                    "Warning a file was found that does not fit the naming scheme provided!"
+                )
+                print(f"Skipping the file: {file}! Please check if this is correct")
         else:
-            raise Exception(f"File type {os.path.splitext(file)} not supported")
+            raise Exception(
+                f"File type {os.path.splitext(file)} not supported! Please provide the files in JSON format"
+            )
 
-    generate_barcodes(index_path, questions_path, amount, mode, max_length)
+    if "answers_path" not in locals():
+        answers_path = ""
+    if "dimensions_path" not in locals():
+        dimensions_path = ""
+
+    if "questions_path" or "index_path" not in locals():
+        raise Exception(
+            "No questions or inverted index found in files! These files are needed in "
+            "order to generate the barcodes!"
+        )
+
+    generate_barcodes(
+        index_path, questions_path, dimensions_path, answers_path, amount, mode
+    )
 
 
 if __name__ == "__main__":
-    amount = 8
-    mode = ""
-    if len(os.listdir(INVERTED_INDEX_PATH)) < 2:
-        raise Exception(
-            "No inverted index or question files found. Please provide both in JSON format"
-        )
-
-    for file in os.listdir(INVERTED_INDEX_PATH):
-        if file.endswith(".json"):
-            if "questions" not in file:
-                index_path = f"../invIndex/{file}"
-            else:
-                questions_path = f"../invIndex/{file}"
-        else:
-            raise Exception(f"File type {os.path.splitext(file)} not supported")
-
-    generate_barcodes(index_path, questions_path, amount, mode, max_length=100)
+    handler(8, "")
