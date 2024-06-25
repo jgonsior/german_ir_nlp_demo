17924656dec7e346c45e96fc6a180737e07ad350
JEmanuel1806 <janplischke97@gmail.com>
2024-06-25 19:34:06 +0200

    revised inverted index script and enumerator script and added documentation

--- Wissenschaftskommunikations Artikel/latex-files/docs_latex/doc_enumerator.py
+++ Wissenschaftskommunikations Artikel/latex-files/docs_latex/doc_enumerator.py
@@ -1,10 +1,24 @@
+import glob
 import re
 
 
 def add_absatz_comment(input_text):
+    """
+    Adds a %ABSATZ comment after every \newline command in the LaTeX document.
+    This will be used for enumerating the documents and displaying section numbers in latex
+    Some %ABSATZ comments have been already added manually, if not separated by newline
+
+
+    Parameters:
+    input_text (str): The input LaTeX document as a string.
+
+    Returns:
+    str: The modified LaTeX document with %ABSATZ comments added.
+    """
     output_text = ""
     inside_document = False
 
+    # Split the input text by \newline to process each section separately
     sections = re.split(r"(\\newline)", input_text)
 
     for section in sections:
@@ -13,7 +27,7 @@ def add_absatz_comment(input_text):
 
         if inside_document:
             if section.strip() == "\\newline":
-                # Add %Absatz comment after each \newline
+                # Add %ABSATZ comment after each \newline
                 output_text += f"{section}\n%ABSATZ "
             else:
                 output_text += section
@@ -24,23 +38,33 @@ def add_absatz_comment(input_text):
 
 
 def replace_absatz(added_text):
+    """
+    Replaces %ABSATZ comments with marginpar{} commands to enumerate the sections.
+    Will display number next to latex text
+    Also needed later for the inverted index to determine the sections
+
+    Parameters:
+    added_text (str): The input LaTeX document with %ABSATZ comments.
+
+    Returns:
+    str: The modified LaTeX document with marginpar{} commands added.
+    """
     section_counter = 1
     output_text = ""
     inside_document = False
 
-    # Split the input text by %ABSATZ
+    # Split the input text by %ABSATZ to process each section separately
     sections = re.split(r"(%ABSATZ)", added_text)
 
     for section in sections:
-        # Detect \begin{document} and set flag to true
         if not inside_document and "\\begin{document}" in section:
             inside_document = True
 
         if inside_document:
             if section.strip() == "%ABSATZ":
-                # Add \marginpar after each %ABSATZ
+                # Add \marginpar with the section number after each %ABSATZ
                 output_text += f"\\marginpar{{{section_counter}}} "
-                section_counter += 1
+                section_counter += 1  # Increment the section counter
             else:
                 output_text += section
         else:
@@ -50,27 +74,32 @@ def replace_absatz(added_text):
 
 
 def main(input_file, output_file):
-    with open(input_file, "r") as f:
-        input_text = f.read()
+    """
+    Reads the input file, processes it to add %ABSATZ comments and marginpar{} commands
+    and writes the output to the specified file.
+    """
 
-    # Add %Absatz comment to input text
+    with open(input_file, "r") as f:
+        input_text = f.read()  # Read the input file content
+
+    # Add %ABSATZ comments to the input text
     added_text = add_absatz_comment(input_text)
+    # Replace %ABSATZ comments with \marginpar{} commands
     output_text = replace_absatz(added_text)
 
-    # Write output tex file
+    # Write the processed content to the output file
     with open(output_file, "w") as f:
         f.write(output_text)
 
 
 if __name__ == "__main__":
-    # Latex doc is entered manually so far , since only needed for 6 docs
-    # could be extended with user input
-    documents = ["src/files/raw/Dumbledore.tex", "src/files/raw/HarryPotter.tex",
-                 "src/files/raw/Hogwarts.tex",
-                 "src/files/raw/Minerva.tex", "src/files/raw/Snape.tex",
-                 "src/files/raw/Voldemort.tex"]
+    # Use glob to find all .tex files in the specified directory
+    documents = glob.glob("src/files/raw/*.tex")
 
     for doc in documents:
-        input_file = doc
-        output_file = "src/files/enumerated/" + input_file.split("/")[-1].split(".")[0] + "_enumerated.tex"
+        input_file = doc  # Current input file
+        # Construct the output file path
+        output_file = "src/files/enumerated/" + input_file.replace("\\", "/").split("/")[-1].split(".")[
+            0] + "_enumerated.tex"
+        # Process the input file and write the output file
         main(input_file, output_file)
--- Wissenschaftskommunikations Artikel/latex-files/inverted_index_latex/inverted_index.py
+++ Wissenschaftskommunikations Artikel/latex-files/inverted_index_latex/inverted_index.py
@@ -1,43 +1,59 @@
+import glob
 import json
 import re
 from collections import defaultdict
+
+import spacy
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
-import spacy
 
+# Load German language model from spaCy
 nlp = spacy.load("de_core_news_sm")
 
-# No stemming on those terms, assure those appear in the inverted index
+# Terms that should not be stemmed to ensure they appear in the inverted index as they are
 unprocessed_terms = ['harry potter', 'hogwarts', 'albus dumbledore', 'severus snape', 'voldemort', 'minerva mcgonagall',
                      'potter', 'dumbledore', 'albus', 'minerva', 'mcgonagall', 'snape', 'severus', 'quidditch',
                      'ariana']
 
 
-# Preprocessing and stemming of every token from the text (Code taken from backend team and adapted)
-def clean_tokens(tokenlist, normalize: bool = True):
+# Clean and preprocess tokens from the text, taken from backend code and adapted
+def clean_tokens(tokenlist, normalize=True):
+    """
+    Cleans and preprocesses tokens from the text.
+
+    Args:
+    - tokenlist (list): List of tokens to clean and preprocess.
+    - normalize (bool): Whether to normalize tokens (default=True).
+
+    Returns:
+    - list: Cleaned and processed tokens.
+    """
+    # Characters to remove from tokens
     remove_chars = ['"', ',', '.', '[', ']', '{', '}', '(', ')', ':', '-', ';', "'", "!", '“', '„', '&', "''", "'s",
                     '*', '...', '):', '/', '--', '---']
+
+    # Filter tokens based on removal characters
     tokens_filtered = [token.lower() for token in tokenlist if token not in remove_chars]
 
+    # Initialize Snowball stemmer for German
     stemmer = SnowballStemmer("german")
     stop_words = set(stopwords.words("german"))
-    if normalize:
-        tokens = []
-        for token in tokens_filtered:
-            if token in unprocessed_terms:
-                tokens.append(token)  # Keep protected terms unchanged
-            else:
-                if token not in stop_words:
-                    tokens.append(stemmer.stem(token))
-    else:
-        tokens = [token for token in tokens_filtered]
+
+    # Process each token
+    tokens = []
+    for token in tokens_filtered:
+        if token in unprocessed_terms:
+            tokens.append(token)  # Keep protected terms unchanged
+        else:
+            if token not in stop_words:
+                tokens.append(stemmer.stem(token)) if normalize else tokens.append(token)
 
     # Filter out unwanted tokens
     filtered_tokens = []
     for token in tokens:
-        if token == '10³':
+        if token == '10³':  # Skip specific token
             continue
-        if token.isdigit() and (1000 <= int(token) <= 2999):  # Include only valid year numbers
+        if token.isdigit() and (1000 <= int(token) <= 2999):  # Include valid year numbers
             filtered_tokens.append(token)
         if re.match(r'^\d+(\.\d+)?$', token):  # Filter out decimal numbers and numbers with trailing dot
             continue
@@ -45,26 +61,45 @@ def clean_tokens(tokenlist, normalize: bool = True):
             continue
         if len(token) == 1 and not token.isdigit():  # Filter out single letters
             continue
-
         elif not token.isdigit():  # Include non-numeric tokens
             filtered_tokens.append(token)
 
     return filtered_tokens
 
 
-# Extract a passage from the text, defined manually before by an "%ABSATZ" comment
+# Extract passages from input text , needed for enumeration later
 def extract_passages(input_text):
-    # Use regex to split text by \marginpar{number} to identify sections
+    """
+    Extracts passages from input text based on predefined patterns.
+
+    Args:
+    - input_text (str): Input text containing passages.
+
+    Returns:
+    - list: List of passages extracted from input text.
+    """
+    # Split text based on \marginpar{number} to identify sections
     passages = re.split(r'\\marginpar{(\d+)}', input_text)
 
-    # Remove any empty strings resulting from split
+    # Remove empty strings resulting from split
     passages = [p for p in passages if p.strip()]
 
     return passages
 
 
-# Create the inverted index to be used for the latex and json
+# Create inverted index from given files
 def create_inverted_index(files, preprocess=False, normalize=True):
+    """
+    Creates an inverted index from a list of files.
+
+    Args:
+    - files (list): List of file paths containing text to index.
+    - preprocess (bool): Whether to preprocess tokens (default=False).
+    - normalize (bool): Whether to normalize tokens (default=True).
+
+    Returns:
+    - defaultdict: Inverted index mapping terms to documents and sections.
+    """
     inverted_index = defaultdict(lambda: defaultdict(list))
 
     # Regex patterns for ignoring LaTeX commands and text within curly brackets
@@ -78,15 +113,15 @@ def create_inverted_index(files, preprocess=False, normalize=True):
         with open(file, 'r', encoding='utf-8') as f:
             input_text = f.read()
 
-        # Extract passages
+        # Extract passages from the document
         passages = extract_passages(input_text)
 
-        # Iterate over the passages
+        # Process each passage
         for i in range(1, len(passages), 2):
             section_id = int(passages[i])
             text = passages[i + 1]
 
-            # Keep track of already processed sections, to avoid duplicates in the inverted index
+            # Track processed terms to avoid duplicates in the inverted index
             section_terms = []
 
             # Remove LaTeX commands and text within curly brackets
@@ -96,19 +131,30 @@ def create_inverted_index(files, preprocess=False, normalize=True):
             # Tokenize the text into terms
             terms = re.findall(r'\b\w+\b', text.lower())  # Lowercase terms for consistency
 
+            # Clean and preprocess tokens if specified
             if preprocess:
                 terms = clean_tokens(terms, normalize)
 
+            # Add terms to the inverted index
             for term in terms:
                 if term not in section_terms:
-                    inverted_index[term][doc_id].append(section_id)  # Store as int for easier sorting
+                    inverted_index[term][doc_id].append(section_id)
                     section_terms.append(term)
 
     return inverted_index
 
 
-# Abbreviate section numbers that appear consecutively (e.g. 111,112,113 -> 111-113)
+# Abbreviate consecutive section numbers in inverted index (e.g., 111,112,113 -> 111-113)
 def abbreviate_sections(sections):
+    """
+    Abbreviates consecutive section numbers.
+
+    Args:
+    - sections (list): List of section numbers to abbreviate.
+
+    Returns:
+    - str: Abbreviated section numbers as a string.
+    """
     if not sections:
         return ""
 
@@ -117,6 +163,7 @@ def abbreviate_sections(sections):
     range_start = sections[0]
     previous = sections[0]
 
+    # Determine consecutive ranges
     for section in sections[1:]:
         if section == previous + 1:
             previous = section
@@ -128,6 +175,7 @@ def abbreviate_sections(sections):
             range_start = section
             previous = section
 
+    # Add last range
     if range_start == previous:
         ranges.append(f'{range_start}')
     else:
@@ -136,9 +184,17 @@ def abbreviate_sections(sections):
     return ', '.join(ranges)
 
 
+# Generate LaTeX format for inverted index
 def generate_latex_table(inverted_index, output_file):
+    """
+    Generates a LaTeX table from an inverted index.
+
+    Args:
+    - inverted_index (defaultdict): Inverted index mapping terms to documents and sections.
+    - output_file (str): Output file path for the LaTeX table.
+    """
     with open(output_file, 'w', encoding='utf-8') as f:
-        # Write the LaTeX document preamble
+        # Write LaTeX document preamble
         f.write(r'\documentclass[a5paper]{article}' + '\n')
         f.write(r'\usepackage[a5paper, margin=0.5in]{geometry}' + '\n')
         f.write(r'\usepackage{longtable}' + '\n')
@@ -154,7 +210,7 @@ def generate_latex_table(inverted_index, output_file):
         f.write(r'\hline' + '\n')
         f.write(r'\endfirsthead' + '\n')
 
-        # Write the inverted index entries
+        # Write entries for each term in the inverted index
         for term, doc_dict in sorted(inverted_index.items()):
             doc_entries = []
             for doc_id, sections in doc_dict.items():
@@ -163,12 +219,22 @@ def generate_latex_table(inverted_index, output_file):
             f.write(rf'{term} & {doc_entry_str} \\' + '\n')
             f.write(r'\hline' + '\n')
 
-        # Write the LaTeX document end
+        # Write LaTeX document end
         f.write(r'\end{longtable}' + '\n')
         f.write(r'\end{document}' + '\n')
 
 
+# Helper function to convert defaultdict to regular dictionary
 def convert_to_regular_dict(d):
+    """
+    Recursively converts a defaultdict to a regular dictionary.
+
+    Args:
+    - d (defaultdict): defaultdict to convert.
+
+    Returns:
+    - dict: Regular dictionary converted from defaultdict.
+    """
     if isinstance(d, defaultdict):
         d = {k: convert_to_regular_dict(v) for k, v in d.items()}
     if isinstance(d, set):
@@ -176,7 +242,17 @@ def convert_to_regular_dict(d):
     return d
 
 
-def generate_json_file(files, preprocess=False, normalize=True):
+# Generate JSON file from inverted index for barcode generation
+def generate_json_file(files, output_file_json, preprocess=False, normalize=True):
+    """
+    Generates a JSON file from an inverted index.
+
+    Args:
+    - files (list): List of file paths containing text to index.
+    - output_file_json (str): Output file path for the JSON file.
+    - preprocess (bool): Whether to preprocess tokens (default=False).
+    - normalize (bool): Whether to normalize tokens (default=True).
+    """
     inverted_index = defaultdict(list)  # Term/Section
 
     # Regex patterns for ignoring LaTeX commands and text within curly brackets
@@ -185,14 +261,13 @@ def generate_json_file(files, preprocess=False, normalize=True):
         r'\\[a-zA-Z]+'  # LaTeX commands without arguments
     ]
 
-    # Iterate over the list of files with their indices
     for doc_id, file in enumerate(files, start=1):
         with open(file, 'r', encoding='utf-8') as f:
             input_text = f.read()
 
         passages = extract_passages(input_text)
 
-        # Iterate over the passages with their indices
+        # Iterate over the passages
         for i in range(1, len(passages), 2):
             section_id = int(passages[i])
             text = passages[i + 1]
@@ -204,13 +279,15 @@ def generate_json_file(files, preprocess=False, normalize=True):
             terms = re.findall(r'\b\w+\b', text.lower())  # Lowercase terms for consistency
             if preprocess:
                 terms = clean_tokens(terms, normalize)
+
+            # Track terms to avoid duplicates in inverted index
             skip = []
             for term in terms:
                 if term not in skip:
                     inverted_index[term].append(str(doc_id) + "-" + str(section_id))
                     skip.append(term)
 
-    # Convert defaultdict to regular dict and lists
+    # Convert defaultdict to regular dictionary and lists
     regular_data = convert_to_regular_dict(inverted_index)
 
     # Generate JSON file
@@ -218,29 +295,35 @@ def generate_json_file(files, preprocess=False, normalize=True):
         json.dump(regular_data, f, sort_keys=True, ensure_ascii=False, indent=4)
 
 
+# Main function to coordinate indexing and generation
 def main(input_files, output_file_latex, output_file_json, preprocess=False, normalize=True):
+    """
+    Main function to create inverted index, generate LaTeX table, and JSON file.
+
+    Args:
+    - input_files (list): List of file paths containing text to index.
+    - output_file_latex (str): Output file path for the LaTeX table.
+    - output_file_json (str): Output file path for the JSON file.
+    - preprocess (bool): Whether to preprocess tokens (default=False).
+    - normalize (bool): Whether to normalize tokens (default=True).
+    """
     # Create inverted index
     inverted_index = create_inverted_index(input_files, preprocess, normalize)
 
     # Generate LaTeX table with inverted index
     generate_latex_table(inverted_index, output_file_latex)
 
-    # Generate JSON inverted index
-    generate_json_file(input_files, preprocess, normalize)
+    # Generate JSON inverted index for barcodes
+    generate_json_file(input_files, output_file_json, preprocess, normalize)
 
 
 if __name__ == "__main__":
-    input_files = ["src/enumerated/Dumbledore_enumerated.tex", "src/enumerated/HarryPotter_enumerated.tex",
-                   "src/enumerated/Hogwarts_enumerated.tex",
-                   "src/enumerated/Minerva_enumerated.tex", "src/enumerated/Snape_enumerated.tex",
-                   "src/enumerated/Voldemort_enumerated.tex"]
-    # Create the inverted index for latex
+    # Define input files and output paths
+    input_files = glob.glob("../docs_latex/src/files/enumerated/*.tex")
     output_file_latex = "src/index/inverted_index.tex"
+    output_file_json = "../../barcodes/invIndex/inverted_index.json"
 
-    # Additionally create a json version for further usage in the barcode generation
-    output_file_json = "src/index/inverted_index.json"
-
-    # Turn off preprocess and normalization manually if needed (unprocessed/processed index)
+    # Run main function with preprocessing and normalization enabled
     preprocess = True
     normalize = True
     main(input_files, output_file_latex, output_file_json, preprocess, normalize)
