58994cc1ffa55d44797669593ff42d8861dded87
Julius Gonsior <julius@gaenseri.ch>
2025-05-19 16:12:57 +0200

    new prod server

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  baseUrl: "http://141.76.47.17:8080",
-  embeddingColor: '#EF6200'
+  production : true,
+  baseUrl : "http://141.76.47.4:8080",
+  embeddingColor : '#EF6200'
 };
